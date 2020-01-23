01982ec88aa17a9e71005b8dd217a1fc3b16ca44
Will <Wi_llo@hotmail.com>
2020-01-23 16:52:24 +1100

    added redux

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
+import { connect } from 'react-redux';
 
 
 class Dashboard extends Component {
     render() {
+        const {projects} = this.props;
         return (
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList></ProjectList>
+                        <ProjectList projects={projects}></ProjectList>
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications></Notifications>
@@ -18,6 +20,12 @@ class Dashboard extends Component {
             </div>
         );
     }
-}
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+};
+
+export default connect(mapStateToProps)(Dashboard);
