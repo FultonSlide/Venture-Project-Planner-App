05b8e0e291f519cd8af9a808ed82e604d51e3778
William Qoro <Wi_llo@hotmail.com>
2020-01-25 23:03:06 +1100

    added signout functionality

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to="/create" className="grey-text text-darken-2">New Project</NavLink></li>
-            <li><NavLink to="/" className="grey-text text-darken-2">Logout</NavLink></li>
-            <li><NavLink to="/" className="btn btn-floating">WQ</NavLink></li>
+            <li><a onClick={props.signOut} className="grey-text text-darken-2">Logout</a></li>
+            <li><NavLink to="/" className="btn btn-floating z-depth-0">WQ</NavLink></li>
         </ul>
     )
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
