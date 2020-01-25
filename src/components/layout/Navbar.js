2fbd4c07850aa304247049dcd4d336ff275d383b
William Qoro <Wi_llo@hotmail.com>
2020-01-25 23:08:12 +1100

    added full sign in/ sign out functionality

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,22 +5,23 @@ import SignedOutLinks from './SignedOutLinks';
 import Venture from '../../VentureLogo.svg';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    console.log(auth);
+    const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>;
     return (
         <nav className="nav-wrapper z-depth-0">
             <div className="container">
                 <Link to='/' className="brand-logo grey-text text-darken-2"><img src={Venture} className="venture-logo"></img></Link>
-                <SignedInLinks></SignedInLinks>
-                <SignedOutLinks></SignedOutLinks>
+                {links}
             </div>
         </nav>
     )
 };
 
 const mapStateToProps = (state) => {
-    console.log(state);
     return {
-
+        auth: state.firebase.auth
     }
 }
 
