64278d4b60ab70e6e2cf0c078bda43023fed74b7
Will <Wi_llo@hotmail.com>
2020-01-26 16:15:28 +1100

    deployed to firebase

---
+++ dist/precache-manifest.ea3b89194610649041939a6938e4e4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b56488d060dde3d661a3a001dc1724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95bf5210973837976475",
+    "url": "/static/css/main.4668a429.chunk.css"
+  },
+  {
+    "revision": "d5c5b699f83a8ed98cfd",
+    "url": "/static/js/2.5058bb9e.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.5058bb9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95bf5210973837976475",
+    "url": "/static/js/main.fb887e00.chunk.js"
+  },
+  {
+    "revision": "7e8f6ab46aa053b6ee71",
+    "url": "/static/js/runtime-main.605a7877.js"
+  },
+  {
+    "revision": "18af538f48e4795d911941dbca076f74",
+    "url": "/static/media/VentureLogo.18af538f.svg"
+  }
+]);
